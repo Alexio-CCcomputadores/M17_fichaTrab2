260e3e8e4fdd80caf6a7ba9d903a0b296c7fea5d
a18987 <a18987@d1-08-07.dped.local>
2017-02-07 16:23:44 +0000

    Versão Alpha 0.1.5

--- src/m17b_fichatrab2/Ponto2D.java
+++ src/m17b_fichatrab2/Ponto2D.java
@@ -6,6 +6,27 @@ public class Ponto2D {
     
     // Faltam construtor, getters e setters.
     
+    public Ponto2D(){
+        x = 0;
+        y = 0;        
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
     public Ponto2D(int x, int y){
         this.x = x;
         this.y = y;        
@@ -13,7 +34,13 @@ public class Ponto2D {
     
     public double distancia(Ponto2D a){
         // Falta fazer este método.
+        
         return 0.0;
+    }
+
+    @Override
+    public String toString() {
+        return "Ponto2D{" + "x=" + x + ", y=" + y + '}';
     }
     
 }
